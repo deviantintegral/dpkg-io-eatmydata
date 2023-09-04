a374f0cacca823c2469bf5a4c0f8a0df417e5ba1
Andrew Berry <andrew@furrypaws.ca>
2023-09-04 13:22:09 -0400

    Remove images to support systems with small disks

--- bench.sh
+++ bench.sh
@@ -5,6 +5,6 @@
 docker build -t emd-test:latest -f Dockerfile.warmup .
 
 hyperfine -- \
-  'docker build --no-cache -f Dockerfile .' \
-  'docker build --no-cache -f Dockerfile.fsync .' \
-  'docker build --no-cache -f Dockerfile.emd .'
+  'docker build --no-cache -f Dockerfile -t emd-temp:latest . && docker rmi emd-temp:latest' \
+  'docker build --no-cache -f Dockerfile.fsync -t emd-temp:latest . && docker rmi emd-temp:latest' \
+  'docker build --no-cache -f Dockerfile.emd -t emd-temp:latest . && docker rmi emd-temp:latest'
